18ca72c3dacd4a7ed06a750dd9fb3b00503a4e06
vi_vio <77623828+vivio2115@users.noreply.github.com>
2024-06-05 00:38:28 +0200

    Update RightPanel.jsx

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -25,7 +25,7 @@ const RightPanel = () => {
 			_id: "665f35adf243be6874dd2550",
 			username: "CountyofDouglas",
 			fullName: "County of Douglas",
-			profileImg: "https://res.cloudinary.com/dmnwvcqsw/image/upload/v171371803/sebjczu8...", // Example URL
+			profileImg: "https://res.cloudinary.com/dmwmvcqsw/image/upload/v1717515720/h8hf0s5nk7mtthrwq3x6.jpg", // Example URL
 		},
 		{
 			_id: "665f350f5047cc8a5ab262a1",
@@ -75,50 +75,6 @@ const RightPanel = () => {
 							</div>
 						</Link>
 					))}
-				</div>
-				<p className='font-bold'>Who to follow</p>
-				<div className='flex flex-col gap-4'>
-					{isLoading && (
-						<>
-							<RightPanelSkeleton />
-							<RightPanelSkeleton />
-							<RightPanelSkeleton />
-							<RightPanelSkeleton />
-						</>
-					)}
-					{!isLoading && suggestedUsers?.length > 0 ? (
-						suggestedUsers.map((user) => (
-							<Link
-								to={`/profile/${user.username}`}
-								className='flex items-center justify-between gap-4'
-								key={user._id}
-							>
-								<div className='flex gap-2 items-center'>
-									<div className='avatar'>
-										<div className='w-8 rounded-full'>
-											<img src={user.profileImg || "/avatar-placeholder.png"} alt={`${user.fullName}'s avatar`} />
-										</div>
-									</div>
-									<div className='flex flex-col'>
-										<span className='font-semibold tracking-tight truncate w-28'>
-											{user.fullName}
-										</span>
-										<span className='text-sm text-slate-500'>@{user.username}</span>
-									</div>
-								</div>
-								<div>
-									<Link
-										to={`/profile/${user.username}`}
-										className='btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm'
-									>
-										Open
-									</Link>
-								</div>
-							</Link>
-						))
-					) : (
-						<div className='text-sm text-slate-500'>No suggestions available</div>
-					)}
 				</div>
 			</div>
 		</div>
