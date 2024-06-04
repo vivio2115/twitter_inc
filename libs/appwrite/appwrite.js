809abb4ba30d1cba3fbf43de2766c3dd7fba0f17
vi_vio <77623828+vivio2115@users.noreply.github.com>
2024-06-04 15:54:02 +0200

    Delete libs directory

--- libs/appwrite/appwrite.js
+++
@@ -1,9 +0,0 @@
-import { Client } from 'appwrite';
-
-const client = new Client();
-
-client
-  .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT)
-  .setProject(process.env.NEXT_PUBLIC_PROJECT);
-
-export default client;
