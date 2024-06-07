198ba869c9bc4d8d385ee3133bb3344fb061dc36
vi_vio <77623828+vivio2115@users.noreply.github.com>
2024-06-07 15:09:15 +0200

    Delete frontend/src/utils.js

--- frontend/src/utils.js
+++
@@ -1,8 +0,0 @@
-// src/utils.js
-export const verifiedUsers = [
-    "665f35adf243be6874dd2550",
-    "665f350f5047cc8a5ab262a1",
-    "665f8a4afc9b39657ec73d92",
-];
-
-export const isVerified = (userId) => verifiedUsers.includes(userId);
