fd813de7f54a46872826645f504ce7549bcd84ec
vi_vio <77623828+vivio2115@users.noreply.github.com>
2024-06-07 15:29:38 +0200

    🔨 Naprawy

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -2,7 +2,7 @@ import XSvg from "../svgs/X";
 
 import { MdHomeFilled } from "react-icons/md";
 import { IoNotifications } from "react-icons/io5";
-import { FaUser, FaSearch } from "react-icons/fa"; // Import FaSearch for the search icon
+import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
@@ -59,15 +59,7 @@ const Sidebar = () => {
 							<span className='text-lg hidden md:block'>Notifications</span>
 						</Link>
 					</li>
-					<li className='flex justify-center md:justify-start'>
-						<Link
-							to='/search'
-							className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
-						>
-							<FaSearch className='w-6 h-6' />
-							<span className='text-lg hidden md:block'>Search</span>
-						</Link>
-					</li>
+
 					<li className='flex justify-center md:justify-start'>
 						<Link
 							to={`/profile/${authUser?.username}`}
